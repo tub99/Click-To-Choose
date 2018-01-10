eb3da4ae52fa05ea6df937ff35469697b84d29d8
soumya.ghosh <soumya.ghosh@learningmate.com>
2018-01-10 19:45:20 +0530

    data sent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { DataHandler } from './services/data-handler/data-handler.service';
 import { AppRoutingModule } from './app-routing.module';
-import { HttpClientModule } from '@angular/common/http'; 
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { QuestionComponent } from './question/question.component';
 import { AnswerComponent } from './answer/answer.component';
-import { QuestionAnswerComponent } from './question-answer/question-answer.component';
+import { AssignmentComponent } from './assignment/assignment.component';
 
 
 @NgModule({
@@ -14,14 +14,14 @@ import { QuestionAnswerComponent } from './question-answer/question-answer.compo
     AppComponent,
     QuestionComponent,
     AnswerComponent,
-    QuestionAnswerComponent
+    AssignmentComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [DataHandler],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
